6d6942b2d9e6e05be001980c9e69dcfde2a3a02f
marcolarthur <marcolarthur@outlook.com>
2023-09-30 20:20:34 +0100

    Updated protobuf dependencies

--- src/main/java/com/flightcomputer/mecc/Mecc.java
+++ src/main/java/com/flightcomputer/mecc/Mecc.java
@@ -9,9 +9,10 @@ import java.io.IOException;
 public class Mecc {
 	public static void main(String[] args) throws IOException, RPCException {
 		try {
-			Connection connection = Connection.newInstance();
+			Connection connection = Connection.newInstance("connection", "127.0.0.1", 50000, 50001);
 			KRPC krpc = KRPC.newInstance(connection);
 			System.out.println("Connected to kRPC version " + krpc.getStatus().getVersion());
+
 		}catch (java.net.ConnectException e){
 			System.out.println(e.getMessage());
 		}
